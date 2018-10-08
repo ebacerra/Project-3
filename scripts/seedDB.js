5e567e73992cf49fbb3a702be0ee430a226e92c5
Ellen <ellen.bacerra@gmail.com>
2018-10-08 14:20:41 -0500

    update

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -17,58 +17,5 @@ convertExcel(fileName, null, null, (err, guestSeed) => {
         throw err;
       });
   }
-  // db.Guest.remove({})
-  //   .then(() => db.Guest.collection.insertMany(guestSeed))
-  //   .then(data => {
-  //     console.log(data.result.n + " records inserted!");
-  //     process.exit(0);
-  //   })
-  //   .catch(err => {
-  //     console.error(err);
-  //     process.exit(1);
-  //   });
 });
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/project-3");
-
-// const guestSeed = [
-//   {
-//     lastName: "Abern",
-//     firstName: "Andrew",
-//     gender: "Male",
-//     phone: "847-436-9123",
-//     role: "participant",
-//     birthday: "5/15/1994"
-//   },
-//   {
-//     lastName: "Babajoni",
-//     firstName: "Kevin",
-//     gender: "Male",
-//     phone: "847-338-0445",
-//     role: "participant",
-//     birthday: "9/26/1995"
-//   },
-//   {
-//     lastName: "Berg Einhorn",
-//     firstName: "Benjamin",
-//     gender: "Male",
-//     phone: "847-436-9123",
-//     role: "participant",
-//     birthday: "6/2/1995"
-//   },
-//   {
-//     lastName: "Block",
-//     firstName: "Molly",
-//     gender: "Female",
-//     phone: "773-320-0471",
-//     role: "participant",
-//     birthday: "7/4/1993"
-//   },
-//   {
-//     lastName: "Berkowitz",
-//     firstName: "Kayla",
-//     gender: "Female",
-//     phone: "732-403-7118",
-//     role: "participant",
-//     birthday: "3/16/1995"
-//   }
-// ];
