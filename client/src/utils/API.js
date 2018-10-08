ce3f3231728ee9b382088c2b36794c9f7ed5b6fc
Ellen <ellen.bacerra@gmail.com>
2018-10-08 17:57:12 -0500

    fixed api codes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,19 +1,18 @@
 import axios from "axios";
 
 export default {
- 
   getGuests: function() {
-    return axios.get(api/guest");
+    return axios.get("/api/guest");
   },
-  // Gets the book with the given id
+  // Gets the guest with the given id
   getGuest: function(id) {
     return axios.get("/api/guest/" + id);
   },
-  // Deletes the book with the given id
+  // Deletes the guest with the given id
   deleteGuest: function(id) {
     return axios.delete("/api/guest/" + id);
   },
-  // Saves a book to the database
+  // Saves a guest to the database
   saveGuest: function(guestData) {
     return axios.post("/api/guest", guestData);
   }
