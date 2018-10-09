043b5b55dd7fe6ea8d799d76f341205b9c7ed893
Ellen <ellen.bacerra@gmail.com>
2018-10-09 12:14:54 -0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
+import React, { Component } from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
 // Styles
 // CoreUI Icons Set
-import '@coreui/icons/css/coreui-icons.min.css';
+import "@coreui/icons/css/coreui-icons.min.css";
 // Import Flag Icons Set
-import 'flag-icon-css/css/flag-icon.min.css';
+import "flag-icon-css/css/flag-icon.min.css";
 // Import Font Awesome Icons Set
-import 'font-awesome/css/font-awesome.min.css';
+import "font-awesome/css/font-awesome.min.css";
 // Import Simple Line Icons Set
-import 'simple-line-icons/css/simple-line-icons.css';
+import "simple-line-icons/css/simple-line-icons.css";
 // Import Main styles for this application
-import './scss/style.css'
+import "./scss/style.css";
 
 // Containers
-import { DefaultLayout } from './containers';
+import { DefaultLayout } from "./containers";
 // Pages
-import { Login, Page404, Page500, Register } from './views/Pages';
+import { Login, Page404, Page500, Register } from "./components/Pages";
 
 // import { renderRoutes } from 'react-router-config';
 
@@ -26,7 +26,12 @@ class App extends Component {
       <HashRouter>
         <Switch>
           <Route exact path="/login" name="Login Page" component={Login} />
-          <Route exact path="/register" name="Register Page" component={Register} />
+          <Route
+            exact
+            path="/register"
+            name="Register Page"
+            component={Register}
+          />
           <Route exact path="/404" name="Page 404" component={Page404} />
           <Route exact path="/500" name="Page 500" component={Page500} />
           <Route path="/" name="Home" component={DefaultLayout} />
