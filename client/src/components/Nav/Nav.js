fee17c39c499e292c4c2acb3ab41aabd5109dbc0
Xinting Yu <xintingyu@dhcp-10-101-67-61.wireless.northwestern.private>
2018-10-11 15:49:58 -0500

    nav

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,29 +1,84 @@
 import React from "react";
-import "./Nav.css";
+// import "./Nav.css";
+import { Nav, NavItem, Dropdown, DropdownItem, DropdownToggle, DropdownMenu, NavLink } from 'reactstrap';
 
-const Nav = () => (
-  <nav>
+class Nav1 extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.toggle = this.toggle.bind(this);
+      this.state = {
+        dropdownOpen: false
+      };
+    }
+  
+    toggle() {
+      this.setState({
+        dropdownOpen: !this.state.dropdownOpen
+      });
+    }
+  
+    render() {
+      return (
+        <div>
+          <Nav tabs>
+          <NavItem>
+              <NavLink disabled href="#">Flashii</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Newsfeed</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Rooming</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Roster</NavLink>
+            </NavItem>
+            <Dropdown nav isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+              <DropdownToggle nav caret>
+                Add
+              </DropdownToggle>
+              <DropdownMenu>
+                {/* <DropdownItem header>Newsfeed</DropdownItem>
+                <DropdownItem disabled>Rooming</DropdownItem>
+                <DropdownItem>Roster</DropdownItem>
+                <DropdownItem divider /> */}
+                <DropdownItem>Add a New Participant</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
 
-      <div className="navbar" id="navbargroup" herf="/">
-      <div className="logoname">
-      <p id="logo">
-      Flashii
-    </p>
 
-      </div>
+
+
+            
+          </Nav>
+         
+        </div>
+      );
+    }
+  }
+
+//   // <nav>
+
+//   //     <div className="navbar" id="navbargroup" herf="/">
+//   //     <div className="logoname">
+//   //     <p id="logo">
+//   //     Flashii
+//   //   </p>
+
+//   //     </div>
     
   
-      <a href="#">Newsfeed</a>
-      <a href="#">Rooming</a>  
-      <a href="#">Roster</a>  
+//   //     <a href="#">Newsfeed</a>
+//   //     <a href="#">Rooming</a>  
+//   //     <a href="#">Roster</a>  
   
-          <a href="#">Add New Participant</a>  
-          <button type="button" class="btn btn-outline-primary">Logout</button>    
+//   //         <a href="#">Add New Participant</a>  
+//   //         <button type="button" class="btn btn-outline-primary">Logout</button>    
 
-  </div>
+//   // </div>
 
-</nav>
+// </nav>
 
-);
-
-export default Nav;
+export default Nav1;
+;
