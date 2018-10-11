af96063c49d802619a1bb8e422c4abaa360a6ca4
Ellen <ellen.bacerra@gmail.com>
2018-10-11 12:33:17 -0500

    deploy

---
+++ models/participant1.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const participantSchema = new Schema({
+  lastName: { type: String, required: true },
+  firstName: { type: String, required: true },
+  nickName: { type: String, required: false },
+  gender: { type: String, required: true },
+  phone: { type: String, required: true },
+  role: { type: String, required: true },
+  birthday: { type: Date, default: Date.now },
+  room: {
+    type: Schema.Types.ObjectId,
+    ref: "Room"
+  }
+});
+
+const Participant = mongoose.model("Participant", participantSchema);
+
+module.exports = Participant;
