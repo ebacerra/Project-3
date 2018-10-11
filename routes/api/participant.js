449adaedeab6d0c7f6daf4dbd66089e28fe465ee
Sherif Mahmoud <sherif.h.mahmoud@gmail.com>
2018-10-11 12:56:18 -0500

    sorting done

--- routes/api/participant.js
+++ routes/api/participant.js
@@ -4,10 +4,14 @@ const participantController = require("../../controllers/participantController")
 // Matches with "/api/participants"
 router
   .route("/")
-  .get(participantController.findAll)
+  .get(participantController.findAll);
   .post(participantController.create);
 
-  
+router
+  .route("/assignRooms")
+  .post(participantController.assignRooms);
+
+
 // Matches with "/api/books/:id"
 router
   .route("/:id")
