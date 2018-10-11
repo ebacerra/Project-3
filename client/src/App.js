cecc374a2c2e27e8bfa5dddc08f085bf842d3655
Ellen <ellen.bacerra@gmail.com>
2018-10-11 11:21:42 -0500

    added the client folder

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Books from "./pages/Books";
+import Nav from "./components/Nav";
+
+const App = () => (
+  <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Books} />
+        <Route exact path="/books" component={Books} />
+
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
---
+++ client/src/pages/Books/Books.js
@@ -0,0 +1,168 @@
+import React, { Component } from "react";
+import DeleteBtn from "../../components/DeleteBtn";
+// import Jumbotron from "../../components/Jumbotron";
+import API from "../../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../../components/Grid";
+import { List, ListItem } from "../../components/List";
+import { Input, TextArea, FormBtn, Select} from "../../components/Form";
+
+class Books extends Component {
+  state = {
+    books: [],
+    title: "",
+    author: "",
+    synopsis: ""
+  };
+
+  componentDidMount() {
+    this.loadBooks();
+  }
+
+  loadBooks = () => {
+    API.getBooks()
+      .then(res =>
+        this.setState({ books: res.data, title: "", author: "", synopsis: "" })
+      )
+      .catch(err => console.log(err));
+  };
+
+  deleteBook = id => {
+    API.deleteBook(id)
+      .then(res => this.loadBooks())
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.title && this.state.author) {
+      API.saveBook({
+        title: this.state.title,
+        author: this.state.author,
+        synopsis: this.state.synopsis
+      })
+        .then(res => this.loadBooks())
+        .catch(err => console.log(err));
+    }
+  };
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-6">
+            {/* <Jumbotron>
+              <h1>What Books Should I Read?</h1>
+            </Jumbotron> */}
+            <form>
+              <Input
+                value={this.state.firstname}
+                onChange={this.handleInputChange}
+                name="firstname"
+                placeholder="First name (required)"
+              />
+               <Input
+                value={this.state.lasttname}
+                onChange={this.handleInputChange}
+                name="lasttname"
+                placeholder="Last name (required)"
+              />
+              <Input
+                value={this.state.whoareyou}
+                onChange={this.handleInputChange}
+                name="whoareyou"
+                placeholder="Who are you?(required)"
+              />
+              
+
+               <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">Regular link</a>
+              <a class="dropdown-item disabled" href="#">Disabled link</a>
+              <a class="dropdown-item" href="#">Another link</a>
+              </div> 
+              
+
+            <Input
+                value={this.state.Nickname}
+                onChange={this.handleInputChange}
+                name="Nickname"
+                placeholder="Nickname (optional)"
+              />
+              <Input
+                value={this.state.Selectgender}
+                onChange={this.handleInputChange}
+                name="Selectgender"
+                placeholder="Select Gender"
+              />
+
+              <Input
+                value={this.state.Phonenumber}
+                onChange={this.handleInputChange}
+                name="Phonenumber"
+                placeholder="Phone Number"
+              />
+
+              <Input
+                value={this.state.Birthday}
+                onChange={this.handleInputChange}
+                name="Birthday"
+                placeholder="mm/dd/yy"
+              />             
+               <Input
+                value={this.state.email}
+                onChange={this.handleInputChange}
+                name="email"
+                placeholder="Email (required)"
+              />
+
+               <Input
+                value={this.state.password}
+                onChange={this.handleInputChange}
+                name="password"
+                placeholder="Password (required)"
+              />
+
+             
+              <FormBtn
+                disabled={!(this.state.firstname && this.state.lastname)}
+                onClick={this.handleFormSubmit}
+              >
+                Submit
+              </FormBtn>
+            </form>
+
+
+          </Col>
+          <Col size="md-6 sm-12">
+            {/*  */}
+            {this.state.books.length ? (
+              <List>
+                {this.state.books.map(book => (
+                  <ListItem key={book._id}>
+                    <Link to={"/books/" + book._id}>
+                      <strong>
+                        {book.title} by {book.author}
+                      </strong>
+                    </Link>
+                    <DeleteBtn onClick={() => this.deleteBook(book._id)} />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>Who are the new Participants?</h3>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Books;
