dc7683d5e41126a324cb6ba52073f297158440a2
haddassah <haddassah@gmail.com>
2018-10-12 16:03:15 -0500

    sort buttons login page

---
+++ client/src/components/SortButton/SortButton.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { Container, CardBody, Row, Col, Button } from "reactstrap";
+
+class SortButton extends Component {
+  state = {};
+
+  render() {
+    const styles = {
+      imageButton: {
+        width: "150px",
+        height: "150px",
+        background: "transparent"
+      }
+    };
+
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Col className="md-4 mx-auto">
+              <CardBody className="mb-2">
+                <Button outline color="warning">
+                  <h3 class="text-center mb-4">Sort Participants</h3>
+                  <img
+                    onClick={() => console.log("clicked button")}
+                    style={styles.imageButton}
+                    alt="sort"
+                    src="https://i.postimg.cc/prCZYFr4/finger-Snap-Button.png"
+                  />
+                </Button>
+              </CardBody>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default SortButton;
