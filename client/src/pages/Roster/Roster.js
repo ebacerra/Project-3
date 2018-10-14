aad0b17db7e3c66c778340bcd9d956f6d422291f
haddassah <haddassah@gmail.com>
2018-10-13 19:01:20 -0500

    cosmetic changes

--- client/src/pages/Roster/Roster.js
+++ client/src/pages/Roster/Roster.js
@@ -1,12 +1,5 @@
-import React, { Component } from "react";
-import DeleteBtn from "../../components/DeleteBtn";
-import Jumbotron from "../../components/Jumbotron";
+import React from "react";
 import Nav from "../../components/Nav";
-import { Link } from "react-router-dom";
-// import { Col, Row, Container } from "../../components/Grid";
-import { List, ListItem } from "../../components/List";
-import { Input, TextArea, FormBtn, Select } from "../../components/Form";
-// import Card from "../../components/Card";
 import { Card, CardImg, CardBody, Container, Row, Col } from "reactstrap";
 import "./Roster.css";
 import background from "./banner5.jpg";
@@ -15,97 +8,99 @@ const Roster = props => {
   return (
     <div>
       <Nav />
-      <Container
-        className="roomingcard-container"
-        style={{
-          backgroundImage: `url(${background})`,
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat"
-        }}
-      >
-        <Row>
-          <Col className="sm">
-            <Row style={{ maxHeight: 320, maxWidth: 1000 }}>
-              <Col xs="12" md="6">
-                <Card body inverse color="info" className="mb-5 mt-5">
-                  <h3 className="text-center">Room #</h3>
-                  <hr />
-                  <CardBody>
-                    <Row className="align-items-center">
-                      <Col
-                        lg="3"
-                        xs="3"
-                        className="mx-auto text-nowrap text-center px-2 mt-2 mb-2"
-                      >
-                        <CardImg
-                          className="d-block mx-auto rounded-circle img-fluid"
-                          src="http://api.randomuser.me/portraits/women/73.jpg"
-                          alt="Image of Participant"
-                        />
-                      </Col>
-                      <Col>
-                        <p style={{ display: "inline" }}>Last Name,</p>
-                        <p style={{ display: "inline" }}> First Name </p>
-                      </Col>
-                    </Row>
-                    <Row className="align-items-center">
-                      <Col
-                        lg="3"
-                        xs="3"
-                        className="mx-auto text-nowrap text-center px-2 mt-2 mb-2"
-                      >
-                        <CardImg
-                          className="d-block mx-auto rounded-circle img-fluid"
-                          src="http://api.randomuser.me/portraits/women/73.jpg"
-                          alt="Image of Participant"
-                        />
-                      </Col>
-                      <Col>
-                        <p style={{ display: "inline" }}>Last Name,</p>
-                        <p style={{ display: "inline" }}> First Name </p>
-                      </Col>
-                    </Row>
-                    <Row className="align-items-center">
-                      <Col
-                        lg="3"
-                        xs="3"
-                        className="mx-auto text-nowrap text-center px-2 mt-2 mb-2"
-                      >
-                        <CardImg
-                          className="d-block mx-auto rounded-circle img-fluid"
-                          src="http://api.randomuser.me/portraits/women/73.jpg"
-                          alt="Image of Participant"
-                        />
-                      </Col>
-                      <Col>
-                        <p style={{ display: "inline" }}>Last Name,</p>
-                        <p style={{ display: "inline" }}> First Name </p>
-                      </Col>
-                    </Row>
-                    <Row className="align-items-center">
-                      <Col
-                        lg="3"
-                        xs="3"
-                        className="mx-auto text-nowrap text-center px-2 mt-2 mb-2"
-                      >
-                        <CardImg
-                          className="d-block mx-auto rounded-circle img-fluid"
-                          src="http://api.randomuser.me/portraits/women/73.jpg"
-                          alt="Image of Participant"
-                        />
-                      </Col>
-                      <Col>
-                        <p style={{ display: "inline" }}>Last Name,</p>
-                        <p style={{ display: "inline" }}> First Name </p>
-                      </Col>
-                    </Row>
-                  </CardBody>
-                </Card>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </Container>
+      <div className="rosterContainer">
+        <Container
+          className="roomingcard-container"
+          style={{
+            backgroundImage: `url(${background})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat"
+          }}
+        >
+          <Row>
+            <Col className="sm">
+              <Row style={{ maxHeight: 320, maxWidth: 1000 }}>
+                <Col xs="12" md="6">
+                  <Card className="mb-5 mt-5">
+                    <h3 className="text-center">Room #</h3>
+                    <hr />
+                    <CardBody>
+                      <Row className="align-items-center">
+                        <Col
+                          lg="3"
+                          xs="3"
+                          className="mx-auto text-nowrap text-center px-2 mt-2 mb-2"
+                        >
+                          <CardImg
+                            className="d-block mx-auto rounded-circle img-fluid"
+                            src="http://api.randomuser.me/portraits/women/73.jpg"
+                            alt="Image of Participant"
+                          />
+                        </Col>
+                        <Col>
+                          <p style={{ display: "inline" }}>Last Name,</p>
+                          <p style={{ display: "inline" }}> First Name </p>
+                        </Col>
+                      </Row>
+                      <Row className="align-items-center">
+                        <Col
+                          lg="3"
+                          xs="3"
+                          className="mx-auto text-nowrap text-center px-2 mt-2 mb-2"
+                        >
+                          <CardImg
+                            className="d-block mx-auto rounded-circle img-fluid"
+                            src="http://api.randomuser.me/portraits/women/73.jpg"
+                            alt="Image of Participant"
+                          />
+                        </Col>
+                        <Col>
+                          <p style={{ display: "inline" }}>Last Name,</p>
+                          <p style={{ display: "inline" }}> First Name </p>
+                        </Col>
+                      </Row>
+                      <Row className="align-items-center">
+                        <Col
+                          lg="3"
+                          xs="3"
+                          className="mx-auto text-nowrap text-center px-2 mt-2 mb-2"
+                        >
+                          <CardImg
+                            className="d-block mx-auto rounded-circle img-fluid"
+                            src="http://api.randomuser.me/portraits/women/73.jpg"
+                            alt="Image of Participant"
+                          />
+                        </Col>
+                        <Col>
+                          <p style={{ display: "inline" }}>Last Name,</p>
+                          <p style={{ display: "inline" }}> First Name </p>
+                        </Col>
+                      </Row>
+                      <Row className="align-items-center">
+                        <Col
+                          lg="3"
+                          xs="3"
+                          className="mx-auto text-nowrap text-center px-2 mt-2 mb-2"
+                        >
+                          <CardImg
+                            className="d-block mx-auto rounded-circle img-fluid"
+                            src="http://api.randomuser.me/portraits/women/73.jpg"
+                            alt="Image of Participant"
+                          />
+                        </Col>
+                        <Col>
+                          <p style={{ display: "inline" }}>Last Name,</p>
+                          <p style={{ display: "inline" }}> First Name </p>
+                        </Col>
+                      </Row>
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 };
