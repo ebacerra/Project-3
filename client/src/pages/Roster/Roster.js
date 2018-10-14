edea48d561dd1eebfab8c4004aee61866e94f2b8
Ellen <ellen.bacerra@gmail.com>
2018-10-13 20:49:10 -0500

    Renders participants in Roster component

--- client/src/pages/Roster/Roster.js
+++ client/src/pages/Roster/Roster.js
@@ -3,106 +3,90 @@ import Nav from "../../components/Nav";
 import { Card, CardImg, CardBody, Container, Row, Col } from "reactstrap";
 import "./Roster.css";
 import background from "./banner5.jpg";
+import API from "../../utils/API";
+import moment from "moment";
 
-const Roster = props => {
-  return (
-    <div>
-      <Nav />
-      <div className="rosterContainer">
-        <Container
-          className="roomingcard-container"
-          style={{
-            backgroundImage: `url(${background})`,
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat"
-          }}
-        >
-          <Row>
-            <Col className="sm">
-              <Row style={{ maxHeight: 320, maxWidth: 1000 }}>
-                <Col xs="12" md="6">
-                  <Card className="mb-5 mt-5">
-                    <h3 className="text-center">Room #</h3>
-                    <hr />
-                    <CardBody>
-                      <Row className="align-items-center">
-                        <Col
-                          lg="3"
-                          xs="3"
-                          className="mx-auto text-nowrap text-center px-2 mt-2 mb-2"
-                        >
-                          <CardImg
-                            className="d-block mx-auto rounded-circle img-fluid"
-                            src="http://api.randomuser.me/portraits/women/73.jpg"
-                            alt="Image of Participant"
-                          />
-                        </Col>
-                        <Col>
-                          <p style={{ display: "inline" }}>Last Name,</p>
-                          <p style={{ display: "inline" }}> First Name </p>
-                        </Col>
-                      </Row>
-                      <Row className="align-items-center">
-                        <Col
-                          lg="3"
-                          xs="3"
-                          className="mx-auto text-nowrap text-center px-2 mt-2 mb-2"
-                        >
-                          <CardImg
-                            className="d-block mx-auto rounded-circle img-fluid"
-                            src="http://api.randomuser.me/portraits/women/73.jpg"
-                            alt="Image of Participant"
-                          />
-                        </Col>
-                        <Col>
-                          <p style={{ display: "inline" }}>Last Name,</p>
-                          <p style={{ display: "inline" }}> First Name </p>
-                        </Col>
-                      </Row>
-                      <Row className="align-items-center">
-                        <Col
-                          lg="3"
-                          xs="3"
-                          className="mx-auto text-nowrap text-center px-2 mt-2 mb-2"
-                        >
-                          <CardImg
-                            className="d-block mx-auto rounded-circle img-fluid"
-                            src="http://api.randomuser.me/portraits/women/73.jpg"
-                            alt="Image of Participant"
-                          />
-                        </Col>
-                        <Col>
-                          <p style={{ display: "inline" }}>Last Name,</p>
-                          <p style={{ display: "inline" }}> First Name </p>
-                        </Col>
-                      </Row>
-                      <Row className="align-items-center">
-                        <Col
-                          lg="3"
-                          xs="3"
-                          className="mx-auto text-nowrap text-center px-2 mt-2 mb-2"
-                        >
-                          <CardImg
-                            className="d-block mx-auto rounded-circle img-fluid"
-                            src="http://api.randomuser.me/portraits/women/73.jpg"
-                            alt="Image of Participant"
-                          />
-                        </Col>
-                        <Col>
-                          <p style={{ display: "inline" }}>Last Name,</p>
-                          <p style={{ display: "inline" }}> First Name </p>
-                        </Col>
-                      </Row>
-                    </CardBody>
-                  </Card>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        </Container>
+class Roster extends React.Component {
+  state = {
+    participants: []
+  };
+
+  componentDidMount() {
+    API.getParticipants().then(res => {
+      let participants = res.data;
+      this.setState({ participants });
+    });
+  }
+
+  renderParticipants = () => {
+    return this.state.participants.map((participant, idx) => {
+      return (
+        <Card key={idx} className="mb-5 mt-5">
+          <h3 className="text-center">{`${participant.lastName}, ${
+            participant.firstName
+          }`}</h3>
+          <hr />
+          <CardBody>
+            <Row className="align-items-center">
+              <Col
+                lg="3"
+                xs="3"
+                className="mx-auto text-nowrap text-center px-2 mt-2 mb-2"
+              >
+                <CardImg
+                  className="d-block mx-auto rounded-circle img-fluid"
+                  src="http://api.randomuser.me/portraits/women/73.jpg"
+                  alt="Image of Participant"
+                />
+              </Col>
+              <Col>
+                {participant.nickName && (
+                  <p style={{ display: "inline" }}>{`Nickname: ${
+                    participant.nickName
+                  }, `}</p>
+                )}
+                {participant.birthday && (
+                  <p style={{ display: "inline" }}>
+                    {`Birthday: ${moment(participant.birthday).format(
+                      "MM/DD/YYYY"
+                    )}`}{" "}
+                  </p>
+                )}
+              </Col>
+            </Row>
+          </CardBody>
+        </Card>
+      );
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="rosterContainer">
+          <Container
+            className="roomingcard-container"
+            style={{
+              backgroundImage: `url(${background})`,
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat"
+            }}
+          >
+            <Row>
+              <Col className="sm">
+                <Row style={{ maxHeight: 320, maxWidth: 1000 }}>
+                  <Col xs="12" md="6">
+                    {this.renderParticipants()}
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Roster;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,23 +1,29 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
+  // Create a participannt
+  createParticipant: participantData => {
+    return axios.post("/api/participant", participantData);
+  },
+  // Gets all participants
   getParticipants: function() {
     return axios.get("/api/participant");
   },
-  // Gets the book with the given id
+  // Gets the participant with the given id
   getParticipant: function(id) {
-    return axios.get("/api/participants/" + id);
+    return axios.get("/api/participant/" + id);
   },
-  // Deletes the book with the given id
+  // Deletes the participant with the given id
   deleteParticipant: function(id) {
-    return axios.delete("/api/participants/" + id);
+    return axios.delete("/api/participant/" + id);
   },
-  // Saves a book to the database
-  saveParticipant: function(participantData) {
-    return axios.post("/api/participants", participantData);
+  // Saves a participant to the database
+  updateParticipant: function(id, participantData) {
+    return axios.put(`/api/participant/${id}`, participantData);
   },
-
+  assignRooms: () => {
+    return axios.post("/api/participant/assignRooms");
+  },
   // Room functions:
   getRooms: function() {
     return axios.get("/api/room");
